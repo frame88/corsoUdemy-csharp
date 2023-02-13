02482ab685257dd9b08d164c2376ae64f3199c4c
Francesco <fleuzzi97@gmail.com>
2023-02-13 16:41:10 +0100

    inferenza di tipo, type decimal, elevazione a potenza

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// stampa di una stringa
-string s = "ciao mondo";
-Console.WriteLine($" s = {s}");
+float f = 1.25F;
+double d = 130.78;
+decimal dm = 1500.456m;
+double ne = 5e-7; //5*10-7
+var s = "type inference";
+var b = false;
+Console.WriteLine($" f = {f}\n d = {d}\n dm = {dm}\n ne = {ne}\n s = {s}\n b = {b}\n");
