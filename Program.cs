133171d293f3537f2d79205be70abe88325fb192
Francesco <fleuzzi97@gmail.com>
2023-02-13 16:18:08 +0100

    dichiarazione variabili, interpolazione stringhe

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Prima esercitazione
+int a = 500;
+short b = 100;
+sbyte c = -50;
+long d = 100_000_000L;
+int e = 0b10101010;
+ushort f = 0x0A56;
+Console.WriteLine(" a = {0},\n b = {1},\n c = {2},\n d = {3},\n e = {4},\n f = {5},", a,b,c,d,e,f);
+Console.WriteLine($" a = {a},\n b = {b},\n c = {c},\n d = {d},\n e = {e},\n f = {f},");
