f045f192914e88dbee45e50e503aa8ede14cdc46
Francesco <fleuzzi97@gmail.com>
2023-02-13 17:32:03 +0100

    condizione if

--- Program.cs
+++ Program.cs
@@ -6,21 +6,15 @@ namespace Conversione
     {
         static void Main(string[] args)
         {   
-            int x = 10;
-            double y = x;
+            int temperatura = 20;
 
-            float x1 = 12000f;
-            int y1 = (int)x1;
-
-            int y2 = y1++;
-            
-            string z = "12345";
-            int x2 = Convert.ToInt32(z);
-
-            Console.WriteLine($"x = {x} y = {y}");
-            Console.WriteLine($"x1 = {x1} y1 = {y1} ");
-            Console.WriteLine($"y2 = {y2}");
-            Console.WriteLine($"z = {z} x2 = {x2}");
+            if (temperatura > 25){
+                Console.WriteLine($"fa caldo");
+            } else if (temperatura < 15) {
+                Console.WriteLine($"Freddo");        
+            } else {
+                Console.WriteLine("Si sta bene");        
+            }
         }
     }
 }
