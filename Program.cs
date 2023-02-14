3d431f555318bc8247c9aaabee13fbf928f9f24d
Francesco <fleuzzi97@gmail.com>
2023-02-14 10:24:29 +0100

    matrice ricavata da un array 2d, con primi indici == 0

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿// - Nella condizione for, mettere un numero in piu per il termine dell'iterazione
+// - Assegnazione del valore matriciale tramite doppio indice, tipo: arr[i ,j] = x;
+// - Usare Console.Write se si intende stampare valori in un ciclo senza andare a capo
 
+using System;
 namespace Primo 
 {
     class Program
     {
         static void Main(string[] args)
         {   
-            int[] x = new int[3] {5, 15, 56};
-            
-            foreach (int value in x)
-                Console.WriteLine(value); 
+            int[,] arr = new int[5,5];
+            int counter = 0;
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    ++counter;
+                    arr[i, j] = (j == 0) ? 0: counter;
+                }
+            }
+
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
